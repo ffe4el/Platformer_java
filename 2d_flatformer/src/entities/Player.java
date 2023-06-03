6d1dab8fb2bf2f8149dd0430d2d6463b5150f1cb
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-06-03 18:15:05 +0900

    gg

--- 2d_flatformer/src/entities/Player.java
+++ 2d_flatformer/src/entities/Player.java
@@ -423,7 +423,7 @@ public class Player extends Entity {
 			for (int i = 0; i < animations[j].length; i++)
 				animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
 
-		statusBarImg = LoadSave.GetSpriteAtlas(LoadSave.STATUS_BAR);
+//		statusBarImg = LoadSave.GetSpristeAtlas(LoadSave.STATUS_BAR);
 	}
 
 	public void loadLvlData(int[][] lvlData) {
